d2233cc933e1c4bff41fadacdd5cacce12d0c350
SzymonOzog <szymon.ozog@gmail.com>
2025-11-28 17:08:10 +0100

    Different sharding

--- csrc/matmul3.cpp
+++ csrc/matmul3.cpp
@@ -1,9 +1,8 @@
-
 void matmul3(float* a, float* b, float* c, const int M, const int N, const int K)
 {
-    for(int m = 0; m<M; m++)
+    for(int k = 0; k<K; k++)
     {
-        for(int k = 0; k<K; k++)
+        for(int m = 0; m<M; m++)
         {
             float a_reg = a[m*K + k];
             for(int n = 0; n<N; n+=4)
