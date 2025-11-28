2df0787874ff7da626dab8718f1d1be06b53d003
SzymonOzog <szymon.ozog@gmail.com>
2025-11-28 17:31:34 +0100

    Use pointer all the time

--- csrc/matmul5.cpp
+++ csrc/matmul5.cpp
@@ -9,14 +9,14 @@ void matmul5(float* a, float* b, float* c, const int M, const int N, const int K
             a_reg[1] = a[(m+1)*K + k];
             a_reg[2] = a[(m+2)*K + k];
             a_reg[3] = a[(m+3)*K + k];
+            float* b_ptr = &b[k*N];
             for(int n = 0; n<N; n+=4)
             {
-                float* b_ptr = &b[k*N + n];
                 float b_reg[4];
                 b_reg[0] = *b_ptr++;
                 b_reg[1] = *b_ptr++;
                 b_reg[2] = *b_ptr++;
-                b_reg[3] = *b_ptr;
+                b_reg[3] = *b_ptr++;
 
                 c[(m+0)*N + n+0] += a_reg[0] * b_reg[0];
                 c[(m+0)*N + n+1] += a_reg[0] * b_reg[1];
