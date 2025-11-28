39587cf0d996f4ec2d8ad0a78533a92f44a032b0
SzymonOzog <szymon.ozog@gmail.com>
2025-11-28 16:49:32 +0100

    Bench multiple sizes

--- bench.py
+++ bench.py
@@ -2,8 +2,6 @@ import torch
 from torch.utils.cpp_extension import load
 import timeit
 
-M, N, K = (512, 256, 1024)
-M, N, K = (1024, 1024, 1024)
 VARIANTS = 4
 NUM_BENCH = 5
 
@@ -13,17 +11,20 @@ my_ext = load(name="my_ext",
               )
 
 if __name__ == "__main__":
+    sizes = [32, 128, 256, 512]
+    for s in sizes:
+        M, N, K = (s,s,s)
+        a = torch.randn((M, K), dtype=torch.float32)
+        b = torch.randn((K, N), dtype=torch.float32)
 
-    a = torch.randn((M, K), dtype=torch.float32)
-    b = torch.randn((K, N), dtype=torch.float32)
+        out = a@b
+        torch_time = timeit.timeit(lambda: a@b, number=NUM_BENCH)
+        print("\nsize =", s)
+        for variant in range(VARIANTS):
+            c = torch.zeros((M, N), dtype=torch.float32)
+            my_ext.matmul(a, b, c, variant)
 
-    out = a@b
-    torch_time = timeit.timeit(lambda: a@b, number=NUM_BENCH)
-    for variant in range(VARIANTS):
-        c = torch.zeros((M, N), dtype=torch.float32)
-        my_ext.matmul(a, b, c, variant)
+            assert torch.allclose(c, out)
 
-        assert torch.allclose(c, out)
-
-        matmul_time = timeit.timeit(lambda: my_ext.matmul(a, b, c, variant), number=NUM_BENCH)
-        print(f"{torch_time=}, {matmul_time=}, {variant=} performance = {(torch_time/matmul_time)*100:.2f}%")
+            matmul_time = timeit.timeit(lambda: my_ext.matmul(a, b, c, variant), number=NUM_BENCH)
+            print(f"{torch_time=}, {matmul_time=}, {variant=} performance = {(torch_time/matmul_time)*100:.2f}%")
